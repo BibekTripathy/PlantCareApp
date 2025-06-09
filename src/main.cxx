f58f67f4e8d0f22df92094e6ccf872b47b66297e
felixity1917 <77635620+felixity1917@users.noreply.github.com>
2025-06-09 15:03:02 +0530

    feat: implement main method

--- src/main.cxx
+++ src/main.cxx
@@ -3,8 +3,59 @@
 #include <ctime>
 #include "plant.hxx"
 
-int main (int argc, char *argv[]) {
-	std::cout << "Hello, World!\n";
-	// testing credentials
+int main (int argc, char **argv) {
+	std::cout << "Welcome to PlantCareApp.\n";
+	Plants plants;
+	std::string filePath{""};
+	std::cout << "Enter address of database (/path/to/database.db): " << std::flush;
+	// getline(cin, filePath);
+	filePath = "testData/databse.db"; // temporary testing data
+	plants.fetchData(filePath);
+	while (true) {
+		std::cout
+			<< "Enter choice\n"
+			<< "1 - Show Plant Details\n"
+			<< "2 - Add New Plant\n"
+			<< "3 - Edit Plant\n"
+			<< "4 - Remove Plant\n"
+			<< "5 - Save & Exit\n"
+			<< "6 - Exit w/o Saving\n"
+			<< std::flush;
+		char choice{'\0'};
+		std::cin >> choice;
+		std::cin.ignore();
+		std::cout << "\n";
+		switch(choice) {
+			case '1': {
+				plants.showDetails();
+				break;
+			}
+			case '2': {
+				plants.startAddPlant();
+				break;
+			}
+			case '3': {
+				plants.startEditPlant();
+				break;
+			}
+			case '4': {
+				plants.startRemovePlant();
+				break;
+			}
+			case '5': {
+				plants.writeData(filePath);
+				std::cout << "Changes have been Saved.\nThanks for using PlantCareApp." << std::endl;
+				return 0;
+			}
+			case '6': {
+				std::cout << "Exiting w/o Saving.\nThanks for using PlantCareApp." << std::endl;
+				return 0;
+			}
+			default: {
+				std::cerr << "Invalid Choice!\n";
+				break;
+			}
+		}
+	}
 	return 0;
 }
